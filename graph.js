41e88b38a500a75fef18fa4591345efefded4132
shoyasato <otas8070@gmail.com>
2018-09-11 11:06:47 +0900

    neutral画像追加

--- graph.js
+++ graph.js
@@ -197,17 +197,17 @@ function plot_frame(){
     smile_chart.update();
     set_lavel_data(nowframe - constSframe);
 
-　//アノテーション用目印
-  if((nowframe - constSframe) == meji){
-    $("#mejirusi").show();
-    $("#waku").show();
-    var waku_he = $('#oneshot').height();
-    $('#waku').outerHeight(waku_he);
-  }else{
-    $("#mejirusi").hide();
-    $("#waku").hide();}
-  }
-};
+    //アノテーション用目印
+    if((nowframe - constSframe) == meji){
+      $("#mejirusi").show();
+      $("#waku").show();
+      var waku_he = $('#oneshot').height();
+      $('#waku').outerHeight(waku_he);
+    }else{
+      $("#mejirusi").hide();
+      $("#waku").hide();}
+    }
+  };
 });
 
 var canvas;
@@ -414,7 +414,6 @@ function plot_dmcdata(human,Start,End){
       item = item[0];
       let data = item._index;
       meji = item._index;
-      alert(data);
       console.log(item);
       smile_data.lineAtIndex = nowframe - constSframe;//parseInt(nowframe)-1;
       smile_data.lineAtIndex2 = meji;
