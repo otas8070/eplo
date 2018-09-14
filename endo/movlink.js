cf064bb3846e9068bd4b986afa97ff512a1f73b9
Ender-Ender <uver427@gmail.com>
2018-09-14 15:00:26 +0900

    path change

--- endo/movlink.js
+++ endo/movlink.js
@@ -7,62 +7,62 @@ $(function(){
 
     /*     リンク     */
   $('#link1').click(function(){
-    window.open("../img/AU_msl/AU 1 Inner Brow Raiser.mp4","AU1", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 1 Inner Brow Raiser.mp4","AU1", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link2').click(function(){
-    window.open("../img/AU_msl/AU 2 Outer Brow Raiser.mp4","AU2", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 2 Outer Brow Raiser.mp4","AU2", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link4').click(function(){
-    window.open("../img/AU_msl/AU 4 Brow Lower.mp4","AU4", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 4 Brow Lower.mp4","AU4", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link5').click(function(){
-    window.open("../img/AU_msl/AU 5 Upper Lid Raiser.mp4","AU5", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 5 Upper Lid Raiser.mp4","AU5", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link6').click(function(){
-    window.open("../img/AU_msl/AU 6 Cheek Raiser.mp4","AU6", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 6 Cheek Raiser.mp4","AU6", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link7').click(function(){
-    window.open("../img/AU_msl/AU 7 Lid Tightener.mp4","AU7", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 7 Lid Tightener.mp4","AU7", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link9').click(function(){
-    window.open("../img/AU_msl/AU 9 Nose Wrinkler.mp4","AU9", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 9 Nose Wrinkler.mp4","AU9", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link12').click(function(){
-    window.open("../img/AU_msl/AU 12 Lip Corner Puller.mp4","AU12", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 12 Lip Corner Puller.mp4","AU12", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link16').click(function(){
-    window.open("../img/AU_msl/AU 16 Lower Lip Depressor.mp4","AU16", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 16 Lower Lip Depressor.mp4","AU16", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link20').click(function(){
-    window.open("../img/AU_msl/AU 20 Lip Stretcher.mp4","AU20", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 20 Lip Stretcher.mp4","AU20", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link23').click(function(){
-    window.open("../img/AU_msl/AU 23 Lip Tightener.mp4","AU23", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 23 Lip Tightener.mp4","AU23", "top=50,width=500,height=500");
     return false;
   });
 
   $('#link26').click(function(){
-    window.open("../img/AU_msl/AU 26 Jaw Drop.mp4","AU26", "top=50,width=500,height=500");
+    window.open("/emo_js/endo/AU_msl/AU 26 Jaw Drop.mp4","AU26", "top=50,width=500,height=500");
     return false;
   });
 
